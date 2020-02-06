af09cb217a7f3bf673366717391f16a9957a482d
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-02-07 00:35:28 +0200

    Is in control.

--- MrConcrete/src/app/views/dashboard/dashboard-nav/dashboard-nav.component.ts
+++ MrConcrete/src/app/views/dashboard/dashboard-nav/dashboard-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-nav',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-nav.component.scss']
 })
 export class DashboardNavComponent implements OnInit {
-
+  @Output() toggleNav: EventEmitter<any> = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleNave() {
+    this.toggleNav.emit(true);
   }
 
 }
