72ebe9e99c4d12449c52b2dc666e439e522f3f30
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-02-08 23:19:09 +0200

    Fix banner button must show

--- MrConcrete/src/app/_services/account.service.ts
+++ MrConcrete/src/app/_services/account.service.ts
@@ -10,6 +10,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AccountService {
+  // tslint:disable-next-line: variable-name
   private _user: BehaviorSubject<UserModel>;
   private user: Observable<UserModel>;
   url: string;
@@ -27,24 +28,24 @@ export class AccountService {
 
   signIn(data: SignInModel): Observable<UserModel> {
     return this.http.post<UserModel>(`${this.url}/api/users/sign-in.php`, data)
-    .pipe(map(response => {
-      if (response) {
-        localStorage.setItem(CURRENT_USER, JSON.stringify(response));
-        this._user.next(response);
-      }
-      return response;
-    }));
+      .pipe(map(response => {
+        if (response) {
+          localStorage.setItem(CURRENT_USER, JSON.stringify(response));
+          this._user.next(response);
+        }
+        return response;
+      }));
   }
 
   signUp(data: SignUpModel): Observable<UserModel> {
     return this.http.post<UserModel>(`${this.url}/api/users/sign-up.php`, data)
-    .pipe(map(response => {
-      if(response) {
-        localStorage.setItem(CURRENT_USER, JSON.stringify(response));
-        this._user.next(response);
-      }
-      return response;
-    }));
+      .pipe(map(response => {
+        if (response) {
+          localStorage.setItem(CURRENT_USER, JSON.stringify(response));
+          this._user.next(response);
+        }
+        return response;
+      }));
   }
   signOut() {
     localStorage.removeItem(CURRENT_USER);
