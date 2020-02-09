6a5509c0a8de62fa8cc74944d7ea595816055848
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2020-02-09 20:05:11 +0200

    view order

--- MrConcrete/src/app/views/dashboard/orders/view-order/view-order.component.ts
+++ MrConcrete/src/app/views/dashboard/orders/view-order/view-order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderView } from 'src/app/_models/orderview.model';
+import { ConcreteorderService } from 'src/app/_services/dashboard';
+import { AccountService } from 'src/app/_services';
+import { UserModel } from 'src/app/_models';
 
 @Component({
   selector: 'app-view-order',
@@ -12,9 +17,18 @@ export class ViewOrderComponent implements OnInit {
     link: '/dashboard/orders',
     label: 'Return to orders'
   };
-  constructor() { }
+
+  order$: Observable<OrderView>;
+  currentUser: UserModel;
+
+  constructor(private concreteorderService: ConcreteorderService,
+    private accountService: AccountService,
+
+  ) { }
 
   ngOnInit() {
+    this.currentUser = this.accountService.CurrentUserValue;
+    this.order$ = this.concreteorderService.order;
   }
 
 }
