b1839a34da46ac754b410483fc481e029ec95d66
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-06-03 21:00:28 +0200

    add table container so that the application does not shake on scroll.

--- src/app/views/dashboard/employees/employees.component.ts
+++ src/app/views/dashboard/employees/employees.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.scss']
 })
-export class EmployeesComponent implements OnInit {
-
+export class EmployeesComponent implements OnInit, OnDestroy {
+  heading = 'Employees';
+  subheading = 'List of employees on the system';
+  actionButton: any = {
+    link: '/dashboard/employees',
+    label: 'Add employee'
+  };
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 
 }
