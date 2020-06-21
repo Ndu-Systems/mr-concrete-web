0537c17e091932c4fa0ec61096c37b364d2e8279
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-06-21 13:59:31 +0200

    role permission for navigation bar

--- src/app/views/dashboard/dashboard-side-nav/dashboard-side-nav.component.ts
+++ src/app/views/dashboard/dashboard-side-nav/dashboard-side-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/_services';
 import { UserModel } from 'src/app/_models';
+import { CUSTOMER_ROLE, ADMIN_ROLE, SUPPLIER_ROLE, DRIVER_ROLE } from '../shared';
 
 @Component({
   selector: 'app-dashboard-side-nav',
@@ -10,15 +11,39 @@ import { UserModel } from 'src/app/_models';
 export class DashboardSideNavComponent implements OnInit {
   currentUser: UserModel;
   isCustomer: boolean;
+  isSupplier: boolean;
+  isAdmin: boolean;
+  isDriver: boolean;
   constructor(
     private accountService: AccountService
   ) { }
 
   ngOnInit() {
     this.currentUser = this.accountService.CurrentUserValue;
-    this.isCustomer = this.currentUser.Roles.RoleName === 'Customer';
+    this.determineRolePermission();
   }
+
+  determineRolePermission() {
+    switch (this.currentUser.Roles.RoleName) {
+      case ADMIN_ROLE.typeOfUser:
+        this.isSupplier = true;
+        break;
+      case CUSTOMER_ROLE.typeOfUser:
+        this.isCustomer = true;
+        break;
+      case DRIVER_ROLE.typeOfUser:
+        this.isDriver = true;
+        break;
+      case SUPPLIER_ROLE.typeOfUser:
+        this.isAdmin = true;
+        break;
+      default:
+        break;
+    }
+  }
+
   logout() {
     this.accountService.signOut();
   }
+
 }
--- src/app/views/dashboard/shared/constants/user.constants.ts
+++ src/app/views/dashboard/shared/constants/user.constants.ts
@@ -1,32 +1,37 @@
 export const USER_TYPES: any[] = [
-  {
-    Id: '1',
-    typeOfUser: 'Admin',
-    Description: 'Has super user privileges, contact support for more info'
-  },
-  {
-    Id: '2',
-    typeOfUser: 'Engineer',
-    Description: 'Classified as a customer, contact support for more info'
-  },
-  {
-    Id: '3',
-    typeOfUser: 'Supplier',
-    Description: 'Classified as the service provider, contact support for more info'
-  },
-  {
-    Id: '4',
-    typeOfUser: 'Assistant',
-    Description: 'Part of the employee personnel, contact support for more info'
-  },
-  {
-    Id: '5',
-    typeOfUser: 'Customer',
-    Description: 'Able to enquire, place orders in the system, contact support for more info'
-  },
-  {
-    Id: '6',
-    typeOfUser: 'Driver',
-    Description: 'Part of the employee personnel, contact support for more info'
-  },
+
+
 ];
+
+export const ADMIN_ROLE = {
+  Id: '1',
+  typeOfUser: 'Admin',
+  Description: 'Has super user privileges, contact support for more info'
+};
+export const ENGINEER_ROLE = {
+  Id: '2',
+  typeOfUser: 'Engineer',
+  Description: 'Classified as a customer, contact support for more info'
+};
+export const SUPPLIER_ROLE = {
+  Id: '3',
+  typeOfUser: 'Supplier',
+  Description: 'Classified as the service provider, contact support for more info'
+};
+export const GENERAL_ROLE = {
+  Id: '4',
+  typeOfUser: 'General',
+  Description: 'Part of the employee personnel, contact support for more info'
+};
+export const CUSTOMER_ROLE = {
+  Id: '5',
+  typeOfUser: 'Customer',
+  Description: 'Able to enquire, place orders in the system, contact support for more info'
+};
+export const DRIVER_ROLE = {
+  Id: '6',
+  typeOfUser: 'Driver',
+  Description: 'Part of the employee personnel, contact support for more info'
+};
+
+
