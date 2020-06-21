5a5bc0dfeb41a803b6429d697389333820ddbd63
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-06-21 23:10:13 +0200

    depoyment updates

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { AccountService } from 'src/app/_services';
 import { Router } from '@angular/router';
 import { UserModel } from 'src/app/_models';
 import { Message } from 'primeng/api/message';
+import { ADMIN_ROLE, CUSTOMER_ROLE, DRIVER_ROLE, SUPPLIER_ROLE } from './shared';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,7 +18,10 @@ export class DashboardComponent implements OnInit {
   showFiller = false;
   @Input() messages: Message[] = [];
   private _mobileQueryListener: () => void;
-
+  isCustomer: boolean;
+  isSupplier: boolean;
+  isAdmin: boolean;
+  isDriver: boolean;
   constructor(
     private accountService: AccountService,
     private routeTo: Router,
@@ -31,8 +35,28 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.currentUser = this.accountService.CurrentUserValue;
+    this.determineRolePermission();
+
   }
 
+  determineRolePermission() {
+    switch (this.currentUser.Roles.RoleName) {
+      case ADMIN_ROLE.typeOfUser:
+        this.isSupplier = true;
+        break;
+      case CUSTOMER_ROLE.typeOfUser:
+        this.isCustomer = true;
+        break;
+      case DRIVER_ROLE.typeOfUser:
+        this.isDriver = true;
+        break;
+      case SUPPLIER_ROLE.typeOfUser:
+        this.isAdmin = true;
+        break;
+      default:
+        break;
+    }
+  }
   signOut() {
     this.accountService.signOut();
     localStorage.clear();
