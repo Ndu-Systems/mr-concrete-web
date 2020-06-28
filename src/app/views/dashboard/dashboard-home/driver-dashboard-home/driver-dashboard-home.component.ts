2a85a64b0d4420f00208bf093a2895ee02b2fcfe
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-06-28 10:26:37 +0200

    Return driver deliveries.

--- src/app/views/dashboard/dashboard-home/driver-dashboard-home/driver-dashboard-home.component.ts
+++ src/app/views/dashboard/dashboard-home/driver-dashboard-home/driver-dashboard-home.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { DeliveryModel, UserModel, DeliveryQueryModel } from 'src/app/_models';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DeliveryService, AccountService } from 'src/app/_services';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-driver-dashboard-home',
   templateUrl: './driver-dashboard-home.component.html',
   styleUrls: ['../dashboard-home.component.scss']
 })
-export class DriverDashboardHomeComponent implements OnInit {
+export class DriverDashboardHomeComponent implements OnInit, OnDestroy  {
   heading = 'Your order deliveries';
   subheading = 'A list of deliveries assigned to you';
   actionButton: any = {
@@ -13,9 +17,29 @@ export class DriverDashboardHomeComponent implements OnInit {
     label: 'View all deliveries'
   };
   today: Date;
-  constructor() { }
+  private onDestroy$ = new Subject<boolean>();
+  deliveries: DeliveryModel[] = [];
+  currentUser: UserModel;
+
+  constructor(private deliveryService: DeliveryService,     private accountService: AccountService) { }
 
   ngOnInit() {
+    this.currentUser = this.accountService.CurrentUserValue;
+    this.getDriverDeliveries();
   }
 
+  ngOnDestroy() {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+
+  getDriverDeliveries() {
+    const query: DeliveryQueryModel = { UserId: this.currentUser.UserId, IsDeleted: false, StatusId: '1' };
+    this.deliveryService.getDriverDeliveries(query);
+    this.deliveryService.deliveries.pipe(takeUntil(this.onDestroy$)).subscribe(data => {
+      if (data) {
+        this.deliveries = data;
+      }
+    });
+  }
 }
